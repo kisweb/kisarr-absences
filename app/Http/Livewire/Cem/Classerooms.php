1fe2ac216c0148e78acb00872780cb8ed4ddc98f
Ibrahima Sarr <kisarrweb@gmail.com>
2021-12-07 12:21:16 +0100

    Second commit

---
+++ app/Http/Livewire/Cem/Classerooms.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Cem;
+
+use Livewire\Component;
+
+class Classerooms extends Component
+{
+    public function render()
+    {
+        return view('livewire.cem.classerooms');
+    }
+}
---
+++ database/migrations/2021_12_06_232417_create_classerooms_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClasseroomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classerooms', function (Blueprint $table) {
+            $table->id();
+            $table->integer('refClasse');
+            $table->string('libClasse');
+            $table->string('niveau');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classerooms');
+    }
+}
